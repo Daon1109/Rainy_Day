79150d3e8a042ec98a71c75e730d0b3cd961fb74
Daon1109 <86394944+Daon1109@users.noreply.github.com>
2022-11-06 23:50:21 +0900

    color added
    with design transplant

--- design.py
+++ design.py
@@ -6,11 +6,14 @@ white = (255,255,255)
 black = (0,0,0)
 blue = (21,129,230)
 red = (227,46,14)
-darkblue = (11,71,128)
 orangeyellow = (250,214,37)
 darkgreen = (8,102,43)
 lightgreen = (8,207,82)
 gray = (158, 171, 176)
+bluegray = (107, 122, 153)
+darkblue = (59, 82, 130)
+lightblue = (140, 167, 219)
+whiteblue = (185, 210, 250)
 color = [0,0,0,0,0]
 
 # font
