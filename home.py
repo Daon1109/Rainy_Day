4e1786729210fbbea096cb553c0371b6eb614911
Daon1109 <86394944+Daon1109@users.noreply.github.com>
2022-11-06 23:51:38 +0900

    new design applied

--- home.py
+++ home.py
@@ -7,20 +7,19 @@ import design as d
 def start_display(SCREEN, clock):
     pygame.display.set_caption("Rainy Day - start")
 
-
-    # design
-    title_font = d.arial(70)
+    # design_update
+    titlepic = pygame.image.load("C:/Coding/Python/Rainy_Day/images/title.png")
+    titlepic = pygame.transform.scale(titlepic, (800, 500))
+    title_rect = titlepic.get_rect()
+    title_rect.centerx = 711
+    title_rect.centery = 300
     strt_font = d.arial(45)
 
-    titlestr = title_font.render("Rainy Day", True, d.black)
-    title_rect = titlestr.get_rect()
-    title_rect.centerx = 711
-    title_rect.centery = 250
 
     s_playing = True
     while s_playing:
 
-        SCREEN.fill(d.white)
+        SCREEN.fill(d.bluegray)
 
         # Event Process
         for event in pygame.event.get():
@@ -32,27 +31,26 @@ def start_display(SCREEN, clock):
 
             # START button
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if pygame.Rect(511, 400, 400, 80).collidepoint(event.pos):
+                if pygame.Rect(461, 520, 500, 80).collidepoint(event.pos):
                     s_playing = False
 
         
         # Rect: Enter button
         # Change button's color by mouse's position
         mousepos = pygame.mouse.get_pos()
-        if mousepos[0]>511 and mousepos[0]<911 and mousepos[1]>400 and mousepos[1]<460:
-            rectcolor = d.lightgreen
+        if mousepos[0]>461 and mousepos[0]<961 and mousepos[1]>520 and mousepos[1]<580:
+            rectcolor = d.lightblue
         else:
-            rectcolor = d.darkgreen
+            rectcolor = d.darkblue
         # Drawing
-        pygame.draw.rect(SCREEN, rectcolor, pygame.Rect(511, 400, 400, 80), border_radius=6)
+        pygame.draw.rect(SCREEN, rectcolor, pygame.Rect(461, 520, 500, 80), border_radius=6)
         enterkey = strt_font.render("START", True, d.white)
         enter_rect = enterkey.get_rect()
         enter_rect.centerx = 711
-        enter_rect.centery = 440
+        enter_rect.centery = 560
         SCREEN.blit(enterkey, enter_rect)
 
-        
-        SCREEN.blit(titlestr, title_rect)
+        SCREEN.blit(titlepic, title_rect)
 
         # Update Screen
         pygame.display.flip()
@@ -83,3 +81,27 @@ def levelchoose(SCREEN, clock):
         pygame.display.flip()
         # fps = 60
         clock.tick(60)
+
+
+def popupScreen(clock):
+    P_SCREEN = pygame.display.set_mode((400,600))
+
+    back_icon = pygame.image.load("C:/Coding/Python/Rainy_Day/images/back.png")
+    pygame.transform.scale(back_icon, (80, 80))
+    back_rect = back_icon.get_rect()
+    back_rect.centerx = 200
+    back_rect.centery = 300
+    replay_icon = pygame.image.load("C:/Coding/Python/Rainy_Day/images/replay.png")
+    pygame.transform.scale(replay_icon, (80, 80))
+    replay_rect = back_icon.get_rect()
+    replay_rect.centerx = 200
+    replay_rect.centery = 450
+
+    playing = True
+    while playing:
+
+        P_SCREEN.fill(d.white)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
